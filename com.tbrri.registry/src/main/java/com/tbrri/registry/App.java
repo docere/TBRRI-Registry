0a018b756527938ac7682002958825704705d045
docere <docere@gmail.com>
2013-07-25 16:50:13 -0400

    First commit

---
+++ com.tbrri.registry/src/main/java/com/tbrri/registry/App.java
@@ -0,0 +1,13 @@
+package com.tbrri.registry;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
